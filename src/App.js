9e44c9981e2432c1002fb559ceab816a6464e935
podvalniypiero <podvalniypiero@mail.ru>
2023-07-21 18:55:04 +0300

    Post component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Header/Header";
 import styles from './index.scss'
+import Post from "./components/Post/Post";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Post/>
+      <div className={styles.container}>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.scss'
+export default function Post() {
+  return (
+    <div className={styles.post__container}>
+      <img
+        src="https://cloud.codesupply.co/endpoint/react/images/post-img-1.jpg"
+        srcset="https://cloud.codesupply.co/endpoint/react/images/post-img-1.jpg 500w, https://cloud.codesupply.co/endpoint/react/images/post-img-1@2x.jpg 1000w"
+        width="360px"
+        alt="img"
+      />
+      <div className={styles.post__container__data}>
+        <h4>Lifestyle</h4>
+        <h2>Eat Right For Your Exercise Regime</h2>
+        <h6>Niek Bove 
+            <span>
+                <svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
+                April 8, 2018
+                <svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
+                3K Views
+            </span>
+        </h6>
+      <p>Structured gripped tape invisible moulded cups for<br/>sauppor firm hold strong powermesh front liner sport <br/> detail. Warmth comfort hangs loosely from the body large <br/> pocket at the front full button…</p>
+      </div>
+    </div>
+  )
+}
