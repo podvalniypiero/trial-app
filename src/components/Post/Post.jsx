115ee6f5d12ab997eefa0434a4f8269f245b64d7
podvalniypiero <podvalniypiero@mail.ru>
2023-07-21 19:23:47 +0300

    fix styles

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -11,7 +11,7 @@ export default function Post() {
       <div className={styles.post__container__data}>
         <h4>Lifestyle</h4>
         <h2>Eat Right For Your Exercise Regime</h2>
-        <ul> <li className={styles.container__data__author}>Niek Bove</li>
+        <ul> <li id={styles.container__data__author}>Niek Bove</li>
             
                 <li><svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
                 April 8, 2018</li>
