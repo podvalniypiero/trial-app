47cb729d812e14ff4dd129fc6ce876560dce8495
podvalniypiero <podvalniypiero@mail.ru>
2023-07-21 19:07:37 +0300

    fix styles

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -11,14 +11,14 @@ export default function Post() {
       <div className={styles.post__container__data}>
         <h4>Lifestyle</h4>
         <h2>Eat Right For Your Exercise Regime</h2>
-        <h6>Niek Bove 
-            <span>
-                <svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
-                April 8, 2018
-                <svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
-                3K Views
-            </span>
-        </h6>
+        <ul> <li className={styles.container__data__author}>Niek Bove</li>
+            
+                <li><svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
+                April 8, 2018</li>
+                <li><svg width="3" height="4" viewBox="0 0 3 4" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="1.5" cy="2" r="1.5" fill="#D7D7D7"/></svg>
+                3K Views</li>
+            
+        </ul>
       <p>Structured gripped tape invisible moulded cups for<br/>sauppor firm hold strong powermesh front liner sport <br/> detail. Warmth comfort hangs loosely from the body large <br/> pocket at the front full button…</p>
       </div>
     </div>
